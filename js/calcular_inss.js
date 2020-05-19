3d0e3a9d1c0908da4c0c263fa80ecf9ee3ab7f1c
evertonweyand <evertonweyand@gmail.com>
2020-05-18 23:08:47 -0300

    Ajustes formatação

--- js/calcular_inss.js
+++ js/calcular_inss.js
@@ -50,63 +50,61 @@ function calcular(){
     var Resultado = 0;
     
     //Faixa 1
+    document.getElementById('fx1_aliquota') .innerText = faixa1_aliq.toLocaleString('pr-BR', {style: 'decimal', currency: 'BRL'})+'%';
+    document.getElementById('fx1_tabinss')  .innerText = faixa1_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
     if (aBC_INSS > 0) {
         var bcFaixa  = casas_decimais_2( Menor_Valor(aBC_INSS, faixa1_vlr).toFixed(3) );
         var vlrFaixa = casas_decimais_2( (bcFaixa * faixa1_aliq / 100 ).toFixed(3) );
         Resultado    = casas_decimais_2( (Resultado + vlrFaixa).toFixed(3), 2);
                 
-        document.getElementById('fx1_aliquota') .innerText = faixa1_aliq;
         document.getElementById('fx1_bc')       .innerText = bcFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
-        document.getElementById('fx1_tabinss')  .innerText = faixa1_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx1_inss')     .innerText = vlrFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx1_acumulado').innerText = Resultado.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         
-        // document.writeln(`BC Faixa1: ${bcFaixa}` +'<br>')
-        // document.writeln(`VL Faixa1: ${vlrFaixa}`+'<br>')
-        // document.writeln(`T  Faixa1: ${Resultado}`+'<br><br>')
     }
     
     //Faixa 2
+    document.getElementById('fx2_aliquota') .innerText = faixa2_aliq.toLocaleString('pr-BR', {style: 'decimal', currency: 'BRL'})+'%';
+    document.getElementById('fx2_tabinss')  .innerText = faixa2_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
     if (aBC_INSS > faixa1_vlr) {
         var bcFaixa  = casas_decimais_2( (Menor_Valor(aBC_INSS, faixa2_vlr) - faixa1_vlr).toFixed(3) );
         var vlrFaixa = casas_decimais_2( (bcFaixa * faixa2_aliq / 100).toFixed(3) );
         Resultado    = casas_decimais_2( (Resultado + vlrFaixa).toFixed(3), 2);
         
-        document.getElementById('fx2_aliquota') .innerText = faixa2_aliq;
         document.getElementById('fx2_bc')       .innerText = bcFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
-        document.getElementById('fx2_tabinss')  .innerText = faixa2_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx2_inss')     .innerText = vlrFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx2_acumulado').innerText = Resultado.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
 
     }
         
     //Faixa 3
+    document.getElementById('fx3_aliquota') .innerText = faixa3_aliq.toLocaleString('pr-BR', {style: 'decimal', currency: 'BRL'})+'%';
+    document.getElementById('fx3_tabinss')  .innerText = faixa3_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});        
     if (aBC_INSS>faixa2_vlr) {
         var bcFaixa  = casas_decimais_2( (Menor_Valor(aBC_INSS, faixa3_vlr) - faixa2_vlr).toFixed(3) );
         var vlrFaixa = casas_decimais_2( (bcFaixa * faixa3_aliq / 100).toFixed(3) );
         Resultado    = casas_decimais_2( (Resultado + vlrFaixa).toFixed(3), 2);
 
-        document.getElementById('fx3_aliquota') .innerText = faixa3_aliq;
         document.getElementById('fx3_bc')       .innerText = bcFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
-        document.getElementById('fx3_tabinss')  .innerText = faixa3_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx3_inss')     .innerText = vlrFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx3_acumulado').innerText = Resultado.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
 
     }
 
     //Faixa 4
+    document.getElementById('fx4_aliquota') .innerText = faixa4_aliq.toLocaleString('pr-BR', {style: 'decimal', currency: 'BRL'})+'%';
+    document.getElementById('fx4_tabinss')  .innerText = faixa4_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
     if (aBC_INSS>faixa3_vlr) {
         var bcFaixa  = casas_decimais_2( (Menor_Valor(aBC_INSS, faixa4_vlr) - faixa3_vlr).toFixed(3) );
         var vlrFaixa = casas_decimais_2( (bcFaixa * faixa4_aliq / 100).toFixed(3) );
         Resultado    = casas_decimais_2( (Resultado + vlrFaixa).toFixed(3), 2);
 
-        document.getElementById('fx4_aliquota') .innerText = faixa4_aliq;
         document.getElementById('fx4_bc')       .innerText = bcFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
-        document.getElementById('fx4_tabinss')  .innerText = faixa4_vlr.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx4_inss')     .innerText = vlrFaixa.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
         document.getElementById('fx4_acumulado').innerText = Resultado.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
 
     }
-    document.getElementById('h2_result').innerText = 'Contribuição devida: '+Resultado.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'});
+
+    document.getElementById('h2_result').innerHTML = 'Contribuição devida ' +'<strong>'+ Resultado.toLocaleString('pr-BR', {style: 'currency', currency: 'BRL'})+'</strong>';
     
 }
